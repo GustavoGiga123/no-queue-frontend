e897e6a4626e2717c33ef7b224d006ae9e6ac3eb
GustavoGiga123 <110988741+GustavoGiga123@users.noreply.github.com>
2022-10-17 13:02:25 +0000

    finalizado

--- pages/finalizacao.vue
+++ pages/finalizacao.vue
@@ -7,7 +7,7 @@
     
       <div class="column is-half mb-1">
         <br>
-
+<section class="section ml-3">
         <h3 class="title is-3"><strong>Check-in realizado com sucesso!</strong></h3>
          <p>Tudo certo com seu check-in Robson!: <strong>O número do seu quarto é: 304</strong></p>
       <br>
@@ -16,6 +16,7 @@
         <p><strong>Você também recebeu o código de barras no seu e-mail.</strong></p>
        <br> 
        <p><strong>OBRIGADO!</strong></p>
+  </section>
     </div>
        </div>
          <b-image style = 'width: 300px;' class="image mt-2" is-6 src="https://files.tecnoblog.net/wp-content/uploads/2018/08/barcode-tecnoblog.png "></b-image>
